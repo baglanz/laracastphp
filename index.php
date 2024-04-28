44e2988ae0f4d2ee49de14603fe735bc29465964
baglanz <baglan.zhaksylyk98@gmail.com>
2024-04-28 19:05:53 +0500

    Conditionals and Booleans

--- index.php
+++ index.php
@@ -2,18 +2,31 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
-    <h1>
-        <?php
-            $greeting = "Hello";
+    <?php
+        $name = "Dark Matter";
+        $read = true;
 
-            echo "$greeting Everybody!";
-        ?>
+        if ($read) {
+            $message = "You have read $name";
+        } else {
+            $message = "You have NOT read $name";
+        }
+    ?>
+
+    <h1>
+        <?= $message ?>
     </h1>
 </body>
 </html>
