05e8f1b9f7a4ab8f71540ca381aac1656bf62437
baglanz <baglan.zhaksylyk98@gmail.com>
2024-04-30 15:47:12 +0500

    Extract a PHP Database Class

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
 //require 'router.php';
 
-$dsn = "mysql:host=localhost;port=3306;dbname=demo;user=root;charset=utf8mb4";
 
-$pdo = new PDO($dsn);
 
-$statement = $pdo->prepare("select * from posts");
-$statement->execute();
+$db = new Database();
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+dd($posts);
