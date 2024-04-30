ccbaa92ecce96c61569e9785f9d185c997a5a503
baglanz <baglan.zhaksylyk98@gmail.com>
2024-04-30 20:53:51 +0500

    Always Escape Untrusted Input

--- views/note.view.php
+++ views/note.view.php
@@ -5,7 +5,7 @@
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             <p class="mb-6"><a href="/notes" class="text-blue-500 underline">Go back...</a></p>
-            <p><?= $note['body'] ?></p>
+            <p><?= htmlspecialchars($note['body']) ?></p>
         </div>
     </main>
 
