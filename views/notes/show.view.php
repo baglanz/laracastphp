38150a1cb1f25c1064f06d65fa5201f47b72e0c8
baglanz <baglan.zhaksylyk98@gmail.com>
2024-05-01 23:26:49 +0500

    Handle Multiple Request Methods From a Controller Action?

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -6,6 +6,10 @@
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             <p class="mb-6"><a href="/notes" class="text-blue-500 underline">Go back...</a></p>
             <p><?= htmlspecialchars($note['body']) ?></p>
+            <form action="#" class="mt-6" method="post">
+                <input type="hidden" name="id" value="<?= $note['id'] ?>">
+                <button class="text-sm text-red-500">Delete</button>
+            </form>
         </div>
     </main>
 
