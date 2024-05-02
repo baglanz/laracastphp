dca19a8d64b6b6de37f8b3fc0c9775ff4092874c
baglanz <baglan.zhaksylyk98@gmail.com>
2024-05-02 23:11:41 +0500

    Updating a Resource With PATCH Requests

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -6,11 +6,11 @@
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             <p class="mb-6"><a href="/notes" class="text-blue-500 underline">Go back...</a></p>
             <p><?= htmlspecialchars($note['body']) ?></p>
-            <form action="#" class="mt-6" method="post">
-                <input type="hidden" name="_method" value="DELETE">
-                <input type="hidden" name="id" value="<?= $note['id'] ?>">
-                <button class="text-sm text-red-500">Delete</button>
-            </form>
+
+            <footer class="mt-6">
+                <a href="/note/edit?id=<?= $note['id'] ?>" class="inline-flex justify-center rounded-md border border-transparent bg-gray-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">Edit</a>
+            </footer>
+
         </div>
     </main>
 
